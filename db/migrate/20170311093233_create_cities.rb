0fe89c9786125b92780ea3864097d1e8285a22cd
Katie Killeen <killeen.katie@gmail.com>
2017-03-11 13:17:51 +0000

    add some styling

--- db/migrate/20170311093233_create_cities.rb
+++ db/migrate/20170311093233_create_cities.rb
@@ -1,9 +1,7 @@
 class CreateCities < ActiveRecord::Migration[5.0]
   def change
     create_table :cities do |t|
-t.string  :title
-t.text :body
-t.references :city, foreign_key: true
+
 
       t.timestamps
     end
