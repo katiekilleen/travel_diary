9dfad5941fbf18ded183d4ece55cf0544808891c
Katie Killeen <killeen.katie@gmail.com>
2017-03-11 11:25:01 +0000

    create reviews

--- db/migrate/20170311110203_create_reviews.rb
+++ db/migrate/20170311110203_create_reviews.rb
@@ -1,10 +1,10 @@
-class CreateCities < ActiveRecord::Migration[5.0]
+class CreateReviews < ActiveRecord::Migration[5.0]
   def change
-    create_table :cities do |t|
+    create_table :reviews do |t|
 t.string  :title
 t.text :body
 t.references :city, foreign_key: true
-
+     
       t.timestamps
     end
   end
